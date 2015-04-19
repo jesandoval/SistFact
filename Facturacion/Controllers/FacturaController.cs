1ea2d2502e048a1eb0cd6a2e9c86e903bce69720
jesandoval <jesandoval@tvperu.gob.pe>
2015-04-18 19:34:46 -0500

    Commit inicial
    Creacion del proyecto y primero prototipos de la aplicacion

---
+++ Facturacion/Controllers/FacturaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Facturacion.Controllers
+{
+    public class FacturaController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Listar(string name, string sidx, string sord, int page, int rows)
+        {
+            //int pageIndex = Convert.ToInt32(page) - 1;
+            //int pageSize = rows;
+            //int totalRecords = context.Count();
+            //int totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
+
+
+            var jsonData = new
+            {
+                total = 3,
+                page = page,
+                records = 3,
+                //rows = new[]{    
+                //            new {i= 1, cell= new string[] { "23450", "PUBLICIDAD", "12/03/2015", "SOLES", "12,345.00" } },
+                //            new {i= 2, cell= new string[] { "23450", "PUBLICIDAD", "12/03/2015", "DÓLARES", "12,345.00" } },
+                //            new {i= 3, cell= new string[] { "23450", "PUBLICIDAD", "12/03/2015", "SOLES", "12,345.00" } }
+                //            }
+            };
+
+            // Return the result in json
+            return Json(jsonData, JsonRequestBehavior.AllowGet);            
+        }
+
+    }
+}
